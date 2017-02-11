8bbfa048c0b6dccdb476093bab2668f5d7f52a40
Peter LaValle <peter.lavalle@gmail.com>
2017-02-11 14:19:50 +0000

    head-stubstring
    new substring function to ease selection of stuff

--- strings.cs
+++ strings.cs
@@ -1,7 +1,9 @@
-﻿/// Peter LaValle / nottingham.ac.uk / gmail.com
+/// Peter LaValle / nottingham.ac.uk / gmail.com
 /// 
 /// Extension methods for string(s)
 /// 
+/// 2017-02-11 ;
+///		- head-stubstring
 /// 2016-11-05 ;
 ///		- better substring
 ///
@@ -14,5 +16,10 @@ public static class strings
 	public static string SubString(this string value, int head, char last)
 	{
 		return value.Substring(head, value.LastIndexOf(last));
+	}
+
+	public static string SubString(this string value, char head)
+	{
+		return value.Substring(value.IndexOf(head) + 1);
 	}
 }

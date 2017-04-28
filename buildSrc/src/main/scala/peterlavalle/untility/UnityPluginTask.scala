77e442111501d08731712b8542cd9497cc76bffc
Unknown <peter.lavalle@gmail.com>
2017-04-29 00:19:41 +0100

    builds an editor DLL and copies project settings

--- buildSrc/src/main/scala/peterlavalle/untility/UnityPluginTask.scala
+++ buildSrc/src/main/scala/peterlavalle/untility/UnityPluginTask.scala
@@ -2,9 +2,11 @@
 package peterlavalle.untility
 
 import java.io.File
+import java.nio.file.Files
 
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
+
 import scala.collection.JavaConversions._
 
 class UnityPluginTask extends TUnityTask {
@@ -25,20 +27,29 @@ class UnityPluginTask extends TUnityTask {
         unityHome / "Editor/Data/Managed/UnityEngine.dll"
     }
 
+  def dllUnityEditor: File =
+    osName match {
+      case "windows" =>
+        unityHome / "Editor/Data/Managed/UnityEditor.dll"
+    }
+
   @TaskAction
   def action(): Unit = {
     require(tempProject.exists() && tempProject.delete() && tempProject.mkdirs())
 
-    // create an empty project
-    invoke(
-      tempProject,
-      List(
-        "-createProject",
-        tempProject.AbsolutePath
-      )
-    ) match {
-      case 0 => ;
-    }
+    // copy our project settings
+    (getProject.getProjectDir **)
+      .filter(_.startsWith("ProjectSettings/"))
+      .foreach {
+        setting =>
+          Files.copy(
+            (getProject.getProjectDir / setting).toPath, {
+              val output = tempProject / setting
+              require(output.getParentFile.exists() || output.getParentFile.mkdirs())
+              output.toPath
+            }
+          )
+      }
 
     // import any/all "other" projects
     config.links.foreach {
@@ -52,20 +63,24 @@ class UnityPluginTask extends TUnityTask {
 
     val unityName: String = getProject.unityName
 
-    val dllPlugin =
-      tempProject / s"Assets/$unityName/Plugin/$unityName.Plugin.dll"
 
+    // compile our Plugin code into a DLL
+    val dllPlugin = tempProject / s"Assets/$unityName/Plugin/$unityName.Plugin.dll"
     require(dllPlugin.getParentFile.exists() || dllPlugin.getParentFile.mkdirs())
 
-    // compile our Plugin code into DLLs
     shellScript(
       List(
         s"${'"' + monoCompiler.getAbsolutePath + '"'}",
         s"-r:${'"' + dllUnityEngine.AbsolutePath + '"'}",
+        config.links.toStream.map {
+          link =>
+            "Link the project plugin.DLL"
+        },
         "-target:library",
 
         (getProject.getProjectDir ** ".*\\.cs")
           .filter(_.startsWith(s"Assets/$unityName/"))
+          .filterNot(_.matches("(^|(.*/))(Demo|Test)/.*"))
           .filterNot(_.contains("/Editor/"))
           .map(getProject.getProjectDir / _),
 
@@ -75,14 +90,52 @@ class UnityPluginTask extends TUnityTask {
       case 0 => ;
     }
 
-    s"Done ; ${dllPlugin.AbsolutePath}" halt
+    // compile our Editor code into a DLL
+    val dllEditor = tempProject / s"Assets/$unityName/Editor/$unityName.Editor.dll"
+    require(dllEditor.getParentFile.exists() || dllEditor.getParentFile.mkdirs())
 
+    shellScript(
+      List(
+        s"${'"' + monoCompiler.getAbsolutePath + '"'}",
+        s"-r:${'"' + dllUnityEngine.AbsolutePath + '"'}",
+        s"-r:${'"' + dllUnityEditor.AbsolutePath + '"'}",
+        s"-r:${'"' + dllPlugin.AbsolutePath + '"'}",
+        config.links.toStream.map {
+          link =>
+            "Link the project plugin.DLL"
+        },
+        config.links.toStream.map {
+          link =>
+            "Link the project editor.DLL"
+        },
+        "-target:library",
 
-    // compile our Editor code into DLLs
-    "compile our Editor code into DLLs" halt
+        (getProject.getProjectDir ** ".*\\.cs")
+          .filter(_.startsWith(s"Assets/$unityName/"))
+          .filterNot(_.matches("(^|(.*/))(Demo|Test)/.*"))
+          .filter(_.contains("/Editor/"))
+          .map(getProject.getProjectDir / _),
+
+        s"-out:${'"' + dllEditor.AbsolutePath + '"'}"
+      )
+    ) match {
+      case 0 => ;
+    }
 
     // copy our assets
-    "copy our assets" halt
+    (getProject.getProjectDir **)
+      .filter(_.startsWith(s"Assets/$unityName/"))
+      .filterNot(_.matches("(^|(.*/))(Demo|Test)/.*"))
+      .filterNot {
+        case code if code.endsWith(".cs.meta") || code.endsWith(".cs") => true
+        case meta if meta.endsWith(".meta") =>
+          (getProject.getProjectDir / meta.dropRight(5)).exists()
+      }
+      .map(">>> " + _)
+      .foreach {
+        asset =>
+          s"TODO ; need the copy-asset code for asset ; $asset" halt
+      }
 
     // perform the final export
     "perform the final export" halt
